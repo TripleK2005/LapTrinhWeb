45584c47feea15f234fce53aa6b70822bf74dd89
TripleK2005 <huy231220800@lms.utc.edu.vn>
2025-10-14 10:57:08 +0700

    Lab09

---
+++ Lab09/Models/Lab09Dbfirst1Context.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab09.Models;
+
+public partial class Lab09Dbfirst1Context : DbContext
+{
+    public Lab09Dbfirst1Context()
+    {
+    }
+
+    public Lab09Dbfirst1Context(DbContextOptions<Lab09Dbfirst1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<CtHoaDon> CtHoaDons { get; set; }
+
+    public virtual DbSet<HoaDon> HoaDons { get; set; }
+
+    public virtual DbSet<KhachHang> KhachHangs { get; set; }
+
+    public virtual DbSet<LoaiSanPham> LoaiSanPhams { get; set; }
+
+    public virtual DbSet<QuanTri> QuanTris { get; set; }
+
+    public virtual DbSet<SanPham> SanPhams { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=TRIPLEK\\SQLEXPRESS;Initial Catalog=Lab09DBFirst1;Integrated Security=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CtHoaDon>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__CT_HOA_D__3214EC275495DCB5");
+
+            entity.ToTable("CT_HOA_DON");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.DonGiaMua).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.HoaDonId).HasColumnName("HoaDonID");
+            entity.Property(e => e.SanPhamId).HasColumnName("SanPhamID");
+            entity.Property(e => e.ThanhTien)
+                .HasComputedColumnSql("([SoLuongMua]*[DonGiaMua])", true)
+                .HasColumnType("decimal(29, 2)");
+            entity.Property(e => e.TrangThai).HasDefaultValue(true);
+
+            entity.HasOne(d => d.HoaDon).WithMany(p => p.CtHoaDons)
+                .HasForeignKey(d => d.HoaDonId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__CT_HOA_DO__HoaDo__66603565");
+
+            entity.HasOne(d => d.SanPham).WithMany(p => p.CtHoaDons)
+                .HasForeignKey(d => d.SanPhamId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__CT_HOA_DO__SanPh__6754599E");
+        });
+
+        modelBuilder.Entity<HoaDon>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__HOA_DON__3214EC279D3BAF89");
+
+            entity.ToTable("HOA_DON");
+
+            entity.HasIndex(e => e.MaHoaDon, "UQ__HOA_DON__835ED13A7130E1F7").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.DiaChi).HasMaxLength(255);
+            entity.Property(e => e.DienThoai).HasMaxLength(15);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.HoTenKhachHang).HasMaxLength(100);
+            entity.Property(e => e.MaHoaDon).HasMaxLength(20);
+            entity.Property(e => e.NgayHoaDon).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.TongTriGia).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.TrangThai).HasDefaultValue(true);
+
+            entity.HasOne(d => d.MaKhachHangNavigation).WithMany(p => p.HoaDons)
+                .HasForeignKey(d => d.MaKhachHang)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__HOA_DON__MaKhach__60A75C0F");
+        });
+
+        modelBuilder.Entity<KhachHang>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__KHACH_HA__3214EC27271C3EDF");
+
+            entity.ToTable("KHACH_HANG");
+
+            entity.HasIndex(e => e.MaKhachHang, "UQ__KHACH_HA__88D2F0E46BF6D2D6").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.DiaChi).HasMaxLength(255);
+            entity.Property(e => e.DienThoai).HasMaxLength(15);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.HoTenKhachHang).HasMaxLength(100);
+            entity.Property(e => e.MaKhachHang).HasMaxLength(20);
+            entity.Property(e => e.MatKhau).HasMaxLength(100);
+            entity.Property(e => e.NgayDangKy).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.TrangThai).HasDefaultValue(true);
+        });
+
+        modelBuilder.Entity<LoaiSanPham>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__LOAI_SAN__3214EC27FD646447");
+
+            entity.ToTable("LOAI_SAN_PHAM");
+
+            entity.HasIndex(e => e.MaLoai, "UQ__LOAI_SAN__730A5758D1A90C32").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.MaLoai).HasMaxLength(20);
+            entity.Property(e => e.TenLoai).HasMaxLength(100);
+            entity.Property(e => e.TrangThai).HasDefaultValue(true);
+        });
+
+        modelBuilder.Entity<QuanTri>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__QUAN_TRI__3214EC2765247E65");
+
+            entity.ToTable("QUAN_TRI");
+
+            entity.HasIndex(e => e.TaiKhoan, "UQ__QUAN_TRI__D5B8C7F09CCEF39B").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.MatKhau).HasMaxLength(100);
+            entity.Property(e => e.TaiKhoan).HasMaxLength(50);
+            entity.Property(e => e.TrangThai).HasDefaultValue(true);
+        });
+
+        modelBuilder.Entity<SanPham>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__SAN_PHAM__3214EC2713117F40");
+
+            entity.ToTable("SAN_PHAM");
+
+            entity.HasIndex(e => e.MaSanPham, "UQ__SAN_PHAM__FAC7442CBBDD14DE").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.DonGia).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.HinhAnh).HasMaxLength(255);
+            entity.Property(e => e.MaSanPham).HasMaxLength(20);
+            entity.Property(e => e.TenSanPham).HasMaxLength(100);
+            entity.Property(e => e.TrangThai).HasDefaultValue(true);
+
+            entity.HasOne(d => d.MaLoaiNavigation).WithMany(p => p.SanPhams)
+                .HasForeignKey(d => d.MaLoai)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__SAN_PHAM__MaLoai__59FA5E80");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
